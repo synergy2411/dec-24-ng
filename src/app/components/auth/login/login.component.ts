abeacc6070a12be9350b2d43af4f1cc37950805e
Sumit Khandelwal <synergy2411@outlook.com>
2024-12-10 16:01:09 +0530

    form classes added

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  onSubmitForm(loginForm: NgForm) {
+    console.log('Email : ', loginForm.value.email);
+    console.log('Password : ', loginForm.value.password);
+  }
 }
