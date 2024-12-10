dda339a05ddcc1516bec1cc141aea36e1a5eb9cd
Sumit Khandelwal <synergy2411@outlook.com>
2024-12-10 16:44:19 +0530

    error messages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
@@ -30,7 +30,7 @@ import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
     LoginComponent,
     SignUpComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/auth/sign-up/sign-up.component.ts
+++ src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
 
+  signUpForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.signUpForm = this.fb.group({
+      email: this.email,
+      password: this.password,
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
 }
