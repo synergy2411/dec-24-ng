458dc37054079251b95a88ea4968ecc11818cbb5
Sumit Khandelwal <synergy2411@outlook.com>
2024-12-12 11:17:11 +0530

    user logged in

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,23 @@ import { NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService) {}
+
   onSubmitForm(loginForm: NgForm) {
     console.log('Email : ', loginForm.value.email);
     console.log('Password : ', loginForm.value.password);
+    this.authService
+      .onUserLogin(loginForm.value.email, loginForm.value.password)
+      .subscribe({
+        next: (data) => {
+          this.errorMessage = '';
+        },
+        error: (err) => {
+          this.errorMessage = err.errorMessage;
+        },
+        complete: () => (this.errorMessage = ''),
+      });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 
 import { initializeApp } from 'firebase/app';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 const firebaseApp = initializeApp({
   apiKey: 'AIzaSyCj5Cu46Bbsbgggui_JVwnAjFHm6y6EmJs',
@@ -29,5 +34,19 @@ export class AuthService {
     }
   }
 
-  onUserLogin(email: string, password: string) {}
+  onUserLogin(email: string, password: string): Observable<{ token: string }> {
+    return new Observable((observer) => {
+      signInWithEmailAndPassword(firebaseAuth, email, password)
+        .then((userCredentials) => {
+          userCredentials.user.getIdToken().then((token) => {
+            console.log('TOKEN : ', token);
+            observer.next({ token });
+            observer.complete();
+          });
+        })
+        .catch((err) => {
+          observer.error({ errorMessage: err.message });
+        });
+    });
+  }
 }
